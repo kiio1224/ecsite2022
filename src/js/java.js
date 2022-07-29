1869dc30c3abf233f42faa5e165105cd7abd57b7
kiio1224 <92066969+kiio1224@users.noreply.github.com>
2022-07-29 14:00:53 +0900

    07/29

--- src/js/java.js
+++ src/js/java.js
@@ -8,3 +8,29 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
     $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
+
+//gazou 
+let imglist = document.querySelectorAll(".itemimg");
+let imgmain = document.querySelector(".imgbox img");
+for(let i=0;i<imglist.length;i++){
+    imglist[i].addEventListener("click",function(){
+        let code = imglist[i].getAttribute("src");
+        imgmain.setAttribute("src",code)
+        
+    })
+}
+//jquery rensyu
+//btn
+var show = 6,
+     add = 4, 
+     target = '.target',
+     btn = $('.btn');
+ $(target + ':nth-child(n + ' + (show + 1) + ')').addClass('hidden');
+
+ btn.on('click', function () {
+   $(target + '.hidden').slice(0, add).removeClass('hidden');
+  
+   if ($(target + '.hidden').length == 0) {
+     btn.fadeOut();
+   }
+ });
